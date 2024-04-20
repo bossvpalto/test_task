dc491a0713ff6cf1b392c8377fb24e5529e26d37
Bossvpalto <Bossvpalto@gmail.com>
2024-04-20 20:24:06 +0300

    Просто перебор массива

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
 
 
+// Task 1
+// Перебор массива и определение страны телефона
+$source_url="https://cdn.jsdelivr.net/gh/andr-04/inputmask-multi@master/data/phone-codes.json";
+$html = file_get_contents($source_url);
+$arCodes=json_decode($html, true);
 
 
+foreach ($arCodes as $code)
+    {
+        print_r($code);
+    }
 
 
 
