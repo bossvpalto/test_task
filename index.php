592c0f9fc08701e5559ec0e631b833484960b6a9
Bossvpalto <Bossvpalto@gmail.com>
2024-04-20 20:39:37 +0300

    Проверка номеров из задания

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 // Task 1
 // Перебор массива и определение страны телефона
 $source_url="https://cdn.jsdelivr.net/gh/andr-04/inputmask-multi@master/data/phone-codes.json";
@@ -8,10 +9,35 @@ $html = file_get_contents($source_url);
 $arCodes=json_decode($html, true);
 
 
-foreach ($arCodes as $code)
+function getCountry($number)
     {
-        print_r($code);
-    }
+        global $arCodes;
+        foreach ($arCodes as $code)
+            {
+                $newcode    =strtr($code['mask'], array (' ' => '','#' => '','-' => '','+' => '','(' => '',')' => ''));
+                $newnumber  =strtr($number  , array (' ' => '','#' => '','-' => '','+' => '','(' => '',')' => ''));
+                if ( substr($newnumber, 0, strlen($newcode)) === $newcode  )
+                    {
+                        return $code['name_ru'];
+                    }
+            }
+}   // end of getCountry
+
+
+echo "+375(29)123-45-67   ";
+echo getCountry("+375(29)123-45-67");
+echo "<br>";
+
+echo "+7 (495) 123 45 67   ";
+echo getCountry("+7 (495) 123 45 67");
+echo "<br>";
+
+echo "7 777 123-45-67   ";
+echo getCountry("7 777 123-45-67");
+echo "<br>";
+
+
+
 
 
 
